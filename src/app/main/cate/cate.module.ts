cca3ca884b62dc5037c3170cc9a7379bf55bad92
VBT\VBT <vubatientien@gmail.com>
2017-08-22 16:18:31 +0700

    update all

---
+++ src/app/main/cate/cate.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CateComponent } from './cate.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CateComponent]
+})
+export class CateModule { }
